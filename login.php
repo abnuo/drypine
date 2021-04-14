cacc80eed06b095c1afeccee9588070b2c998cfc
tatey12 <tatebaldwin788@gmail.com>
2021-04-14 12:31:31 -0400

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="style.css">
-<form action="logmein.php" method="post">
-<div>
- <p>Username : <input type="text" name="username" /></p>
- <p>Password : <input type="password" name="password" /></p>
- <p><input type="submit" value="Log in" /></p>
-</div>
